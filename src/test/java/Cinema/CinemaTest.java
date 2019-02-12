8298d38b73e632c54a7b4f593abf2306482b739c
jimting <handsome841206@gmail.com>
2019-02-12 18:53:55 +0800

    Update CinemaTest.java

--- src/test/java/Cinema/CinemaTest.java
+++ src/test/java/Cinema/CinemaTest.java
@@ -6,25 +6,39 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class CinemaTest {
 
 	private WebDriver driver;		
 		
 	@Before
-	public void beforeTest() {	
-	    driver = new FirefoxDriver();  
+	public void beforeTest() 
+	{	
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+
+		// Tell the Java bindings to use Marionette.
+		// This will not be necessary in the future,
+		// when Selenium will auto-detect what remote end
+		// it is talking to.
+		capabilities.setCapability("marionette", true);
+
+		driver = new RemoteWebDriver(capabilities);
+	    //driver = new FirefoxDriver();  
 	}		
 	
 	@Test				
-	public void testEasy() {	
+	public void testEasy() 
+	{	
 		driver.get("main/resources/static/index.html");  
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Cinema - Home")); 		
 	}
 	
 	@After
-	public void afterTest() {
+	public void afterTest() 
+	{
 		driver.quit();			
 	}		
 
