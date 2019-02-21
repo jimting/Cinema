65a3736c3966fe2ed9583c95a7e8b6cfd540a51b
jimting <handsome841206@gmail.com>
2019-02-21 14:31:19 +0800

    Update CinemaController.java

--- src/main/java/Cinema/CinemaController.java
+++ src/main/java/Cinema/CinemaController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-
 @RestController
 public class CinemaController {
 	
