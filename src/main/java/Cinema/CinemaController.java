8e5738618c271d58b858eacce066aa577a7fb8fc
jimting <handsome841206@gmail.com>
2019-02-21 13:32:48 +0800

    update swagger

--- src/main/java/Cinema/CinemaController.java
+++ src/main/java/Cinema/CinemaController.java
@@ -2,6 +2,9 @@ package Cinema;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -9,18 +12,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
-
-
-
+@Api(value = "CinemaController", tags = "其實就是設定網址前端的轉跳")
 @RestController
 public class CinemaController {
+	@Autowired
 	
+	@ApiOperation(value = "index", notes = "return index.html")
 	@CrossOrigin(origins = "*")
 	@RequestMapping("/")
     public void index(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException 
@@ -30,6 +33,7 @@ public class CinemaController {
 		view.forward(request, response);
     }
 	
+	@ApiOperation(value = "shop", notes = "return shop.html")
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/shop", method = RequestMethod.GET)
     public void shop(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException 
