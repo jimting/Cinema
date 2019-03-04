044d6e82e468d36ea26a4eb7db66e8805497ee70
jimting <handsome841206@gmail.com>
2019-03-04 10:56:24 +0800

    update something about test

--- src/test/java/Cinema/CinemaTest.java
+++ src/test/java/Cinema/CinemaTest.java
@@ -72,33 +72,92 @@ public class CinemaTest {
 	}
 
 	//測試購買新電影
-//	@Test	
-//	public void testOrderNewMovie() 
-//	{	
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.get("http://140.121.196.23:4107/");
-//		//等個幾秒 讓頁面Ajax loading好
-//		waitForPageLoaded();
-//		
-//		//這裡要購買第三部電影 妖貓傳 ID是 : 5c35ddefa3f01c41d8bb4174
-//		
-//		//按電影列表
-//		driver.findElement(By.xpath("//button[@data-target='#MovieListModal']")).click();
-//		
-//		//按妖貓傳的選擇框框
-//		driver.findElement(By.xpath("//input[@value=\"5c35ddefa3f01c41d8bb4174\"]")).click();
-//		
-//		//按下購買Button
-//		driver.findElement(By.xpath("//button[@id='orderBtn']")).click();;
-//		
-//		/*等陳俊佑修好再繼續= =
-//		//拿到alert訊息
-//		Alert alert = driver.switchTo().alert();
-//		String alertText = alert.getText();
-//		System.out.println("Alert訊息 : "+alertText);
-//		Assert.assertTrue(!alertText.contains("error")); */		
-//	}
+	@Test	
+	public void testOrderNewMovie() throws InterruptedException 
+	{	
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://140.121.196.23:4107/");
+		//等個幾秒 讓頁面Ajax loading好
+		waitForPageLoaded();
+		
+		//這裡要購買第三部電影 妖貓傳 ID是 : 5c35ddefa3f01c41d8bb4174
+		
+		//按電影列表
+		driver.findElement(By.xpath("//button[@data-target='#MovieListModal']")).click();
+		System.out.println(driver.findElement(By.xpath("//button[@data-target='#MovieListModal']")).getText());
+		
+		//等個幾秒 讓電影列表跳出來
+		Thread.sleep(3000);
+		
+		//按妖貓傳的選擇框框
+		driver.findElement(By.xpath("//input[@value='5c35ddefa3f01c41d8bb4174']")).click();
+		if(driver.findElement(By.xpath("//input[@name='orderMovie' and @value='5c35ddefa3f01c41d8bb4174']")).isSelected())
+			System.out.println("已經選擇了！");
+		else
+			System.out.println("沒有選擇");
+		//等個幾秒 讓按鈕點下去
+		Thread.sleep(3000);
+		
+		//按下購買Button
+		driver.findElement(By.xpath("//button[@id='orderBtn']")).click();;
+		
+		//等個幾秒 讓購買loading好
+		Thread.sleep(3000);
+				
+		//拿到alert訊息
+		Alert alert = driver.switchTo().alert();
+		String alertText = alert.getText();
+		System.out.println("Alert訊息 : "+alertText);
+		Assert.assertTrue(!alertText.contains("error"));	
+	}
 
+	//測試看看是否有拿到新訊息
+	@Test
+	public void testIndexGetNewNotification()
+	{
+		driver.get("http://140.121.196.23:4107/");
+		//等個幾秒 讓頁面Ajax loading好
+		waitForPageLoaded();
+		//確定新訊息數目>0
+		int notificationCount;
+		try
+		{
+			notificationCount = Integer.parseInt(driver.findElement(By.xpath("//span[@id='unreadMessage']")).getText());
+		}catch (Exception e)
+		{
+			notificationCount = 0;
+		}
+		System.out.println("拿到通知數了！ : "+notificationCount);		
+		//如果拿到的通知數>0 就通過啦
+		Assert.assertTrue(notificationCount>0);
+	}
+	
+	//測試是否可以將訊息設為已讀
+	@Test
+	public void testIndexSetNotificationRead()
+	{
+		driver.get("http://140.121.196.23:4107/");
+		//等個幾秒 讓頁面Ajax loading好
+		waitForPageLoaded();
+		//點擊訊息按鈕
+		driver.findElement(By.xpath("//button[@data-toggle='dropdown']")).click();
+		//重新整理並等待loading
+		driver.navigate().refresh();
+		waitForPageLoaded();
+		//確定新訊息數目==0
+		int notificationCount;
+		try
+		{
+			notificationCount = Integer.parseInt(driver.findElement(By.xpath("//span[@id='unreadMessage']")).getText());
+		}catch (Exception e)
+		{
+			notificationCount = 0;
+		}
+		System.out.println("拿到通知數了！ : "+notificationCount);		
+		//如果確定所有訊息都被設定為已讀，就通過啦
+		Assert.assertTrue(notificationCount==0);
+	}
+	
 	//test shop's title
 	@Test
 	public void testShopTitle()
@@ -131,31 +190,37 @@ public class CinemaTest {
 	}
 	
 	//測試購買新周邊
-//	@Test	
-//	public void testOrderNewGrocery() 
-//	{	
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.get("http://140.121.196.23:4107/shop.html");
-//		//等個幾秒 讓頁面Ajax loading好
-//		waitForPageLoaded();
-//		
-//		//這裡要購買第三個周邊 重低音耳機 input id是 : quantity3
-//		
-//		//按周邊列表
-//		driver.findElement(By.xpath("//button[@data-target='#GoodsListModal']")).click();
-//		
-//		//將重低音耳機的數量設為1
-//		driver.findElement(By.xpath("//input[@id=\"quantity3\"]")).sendKeys("1");
-//		
-//		//按下購買Button
-//		driver.findElement(By.xpath("//button[@id='orderBtn']")).click();;
-//		
-//		//拿到alert訊息
-//		Alert alert = driver.switchTo().alert();
-//		String alertText = alert.getText();
-//		System.out.println("Alert訊息 : "+alertText);
-//		Assert.assertTrue(!alertText.contains("error"));	
-//	}
+	@Test	
+	public void testOrderNewGrocery() throws InterruptedException 
+	{	
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://140.121.196.23:4107/shop.html");
+		//等個幾秒 讓頁面Ajax loading好
+		waitForPageLoaded();
+		
+		//這裡要購買第三個周邊 重低音耳機 input id是 : quantity3
+		
+		//按周邊列表
+		driver.findElement(By.xpath("//button[@data-target='#GoodsListModal']")).click();
+		//等個幾秒 讓列表跳出來
+		Thread.sleep(3000);
+		
+		//將重低音耳機的數量設為1
+		driver.findElement(By.xpath("//input[@id=\"quantity3\"]")).sendKeys("1");
+		//等個幾秒 讓動作完成
+		Thread.sleep(3000);
+		
+		//按下購買Button
+		driver.findElement(By.xpath("//button[@id='orderBtn']")).click();;
+		//等個幾秒 讓購買動作完成
+		Thread.sleep(3000);
+				
+		//拿到alert訊息
+		Alert alert = driver.switchTo().alert();
+		String alertText = alert.getText();
+		System.out.println("Alert訊息 : "+alertText);
+		Assert.assertTrue(!alertText.contains("error"));	
+	}
 	
 	@After
 	public void afterTest() 
