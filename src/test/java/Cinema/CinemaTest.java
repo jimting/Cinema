ba083c3f20d2a4e60169a3d07e977c69ce223c69
jimting <handsome841206@gmail.com>
2019-03-11 11:28:57 +0800

    update something about testing

--- src/test/java/Cinema/CinemaTest.java
+++ src/test/java/Cinema/CinemaTest.java
@@ -10,7 +10,9 @@ import org.json.JSONException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -23,6 +25,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CinemaTest {
 
 	private WebDriver driver;		
@@ -32,7 +35,7 @@ public class CinemaTest {
 	public void beforeTest() 
 	{	
 		//設定要用什麼瀏覽器來測試
-		System.setProperty("webdriver.chrome.driver","src/lib/chromedriver");
+		System.setProperty("webdriver.chrome.driver","src/lib/chromedriver.exe");
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.addArguments("--headless");
 		driver = new ChromeDriver(chromeOptions);
@@ -40,8 +43,8 @@ public class CinemaTest {
 	}		
 
 	//test index's title
-	@Test	
-	public void testIndexTitle() 
+	@Test
+	public void test1_1_IndexTitle() 
 	{	
 		driver.get("http://140.121.196.23:4107/");
 		String title = driver.getTitle();			
@@ -50,7 +53,7 @@ public class CinemaTest {
 
 	//測試是否拿到所有電影資料
 	@Test
-	public void testGetMovieList()
+	public void test1_2_GetMovieList()
 	{
 		//拿到變數 movieData 並確認電影資料
 		// Use the timeout when navigating to a webpage
@@ -73,7 +76,7 @@ public class CinemaTest {
 
 	//測試購買新電影
 	@Test	
-	public void testOrderNewMovie() throws InterruptedException 
+	public void test1_3_OrderNewMovie() throws InterruptedException 
 	{	
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://140.121.196.23:4107/");
@@ -87,7 +90,7 @@ public class CinemaTest {
 		System.out.println(driver.findElement(By.xpath("//button[@data-target='#MovieListModal']")).getText());
 		
 		//等個幾秒 讓電影列表跳出來
-		Thread.sleep(3000);
+		Thread.sleep(500);
 		
 		//按妖貓傳的選擇框框
 		driver.findElement(By.xpath("//input[@value='5c35ddefa3f01c41d8bb4174']")).click();
@@ -96,13 +99,13 @@ public class CinemaTest {
 		else
 			System.out.println("沒有選擇");
 		//等個幾秒 讓按鈕點下去
-		Thread.sleep(3000);
+		Thread.sleep(500);
 		
 		//按下購買Button
 		driver.findElement(By.xpath("//button[@id='orderBtn']")).click();;
 		
 		//等個幾秒 讓購買loading好
-		Thread.sleep(3000);
+		Thread.sleep(500);
 				
 		//拿到alert訊息
 		Alert alert = driver.switchTo().alert();
@@ -113,7 +116,7 @@ public class CinemaTest {
 
 	//測試看看是否有拿到新訊息
 	@Test
-	public void testIndexGetNewNotification()
+	public void test1_4_IndexGetNewNotification()
 	{
 		driver.get("http://140.121.196.23:4107/");
 		//等個幾秒 讓頁面Ajax loading好
@@ -134,7 +137,7 @@ public class CinemaTest {
 	
 	//測試是否可以將訊息設為已讀
 	@Test
-	public void testIndexSetNotificationRead() throws InterruptedException
+	public void test1_5_IndexSetNotificationRead() throws InterruptedException
 	{
 		driver.get("http://140.121.196.23:4107/");
 		//等個幾秒 讓頁面Ajax loading好
@@ -142,12 +145,12 @@ public class CinemaTest {
 		//點擊訊息按鈕
 		driver.findElement(By.id("dropdown")).click();
 		//等個幾秒 讓點擊工作完成
-		Thread.sleep(3000);
+		Thread.sleep(500);
 		
 		//重新整理並等待loading
 		driver.navigate().refresh();
 		//等個幾秒 畫面刷新
-		Thread.sleep(3000);
+		Thread.sleep(500);
 				
 		//確定新訊息數目==0
 		int notificationCount;
@@ -165,7 +168,7 @@ public class CinemaTest {
 	
 	//test shop's title
 	@Test
-	public void testShopTitle()
+	public void test2_1_ShopTitle()
 	{
 		driver.get("http://140.121.196.23:4107/shop.html");
 		String title = driver.getTitle();			
@@ -174,7 +177,7 @@ public class CinemaTest {
 	
 	//測試是否拿到所有周邊資料
 	@Test
-	public void testGetGroceryList()
+	public void test2_2_GetGroceryList()
 	{
 		//拿到變數 movieData 並確認電影資料
 		// Use the timeout when navigating to a webpage
@@ -196,7 +199,7 @@ public class CinemaTest {
 	
 	//測試購買新周邊
 	@Test	
-	public void testOrderNewGrocery() throws InterruptedException 
+	public void test2_3_OrderNewGrocery() throws InterruptedException 
 	{	
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://140.121.196.23:4107/shop.html");
@@ -208,17 +211,17 @@ public class CinemaTest {
 		//按周邊列表
 		driver.findElement(By.xpath("//button[@data-target='#GoodsListModal']")).click();
 		//等個幾秒 讓列表跳出來
-		Thread.sleep(3000);
+		Thread.sleep(500);
 		
 		//將重低音耳機的數量設為1
 		driver.findElement(By.xpath("//input[@id=\"quantity3\"]")).sendKeys("1");
 		//等個幾秒 讓動作完成
-		Thread.sleep(3000);
+		Thread.sleep(500);
 		
 		//按下購買Button
 		driver.findElement(By.xpath("//button[@id='orderBtn']")).click();;
 		//等個幾秒 讓購買動作完成
-		Thread.sleep(3000);
+		Thread.sleep(500);
 				
 		//拿到alert訊息
 		Alert alert = driver.switchTo().alert();
@@ -227,6 +230,103 @@ public class CinemaTest {
 		Assert.assertTrue(!alertText.contains("error"));	
 	}
 	
+	//測試看看是否有拿到新訊息
+	@Test
+	public void test2_4_ShopGetNewNotification()
+	{
+		driver.get("http://140.121.196.23:4107/shop.html");
+		//等個幾秒 讓頁面Ajax loading好
+		waitForPageLoaded();
+		//確定新訊息數目>0
+		int notificationCount;
+		try
+		{
+			notificationCount = Integer.parseInt(driver.findElement(By.xpath("//span[@id='unreadMessage']")).getText());
+		}catch (Exception e)
+		{
+			notificationCount = 0;
+		}
+		System.out.println("拿到通知數了！ : "+notificationCount);		
+		//如果拿到的通知數>0 就通過啦
+		Assert.assertTrue(notificationCount>0);
+	}
+		
+	//測試是否可以將訊息設為已讀
+	@Test
+	public void test2_5_ShopSetNotificationRead() throws InterruptedException
+	{
+		driver.get("http://140.121.196.23:4107/shop.html");
+		//等個幾秒 讓頁面Ajax loading好
+		waitForPageLoaded();
+		//點擊訊息按鈕
+		driver.findElement(By.id("dropdown")).click();
+		//等個幾秒 讓點擊工作完成
+		Thread.sleep(500);
+			
+		//重新整理並等待loading
+		driver.navigate().refresh();
+		//等個幾秒 畫面刷新
+		Thread.sleep(500);
+					
+		//確定新訊息數目==0
+		int notificationCount;
+		try
+		{
+			notificationCount = Integer.parseInt(driver.findElement(By.xpath("//span[@id='unreadMessage']")).getText());
+		}catch (Exception e)
+		{
+			notificationCount = 0;
+		}
+		System.out.println("拿到通知數了！ : "+notificationCount);		
+		//如果確定所有訊息都被設定為已讀，就通過啦
+		Assert.assertTrue(notificationCount==0);
+	}
+	
+//	//測試是否拿到所有已購買電影資料
+//	@Test
+//	public void test1_6_GetMovieOrderedList()
+//	{
+//		//拿到變數 orderedListData 並確認電影資料
+//		// Use the timeout when navigating to a webpage
+//		driver.get("http://140.121.196.23:4107/");
+//		//等個幾秒 讓頁面Ajax loading好
+//		waitForPageLoaded();
+//		try {
+//			//拿到了
+//			JSONArray orderedListData = new JSONArray((String)js.executeScript("return JSON.stringify(orderedListData);"));
+//			System.out.println((String)js.executeScript("return JSON.stringify(orderedListData);"));
+//			//如果第3筆資料的電影名稱是 "妖貓傳" ，就通過
+//			Assert.assertTrue(orderedListData.getJSONObject(orderedListData.length()).getString("MovieName").contains("妖貓傳"));
+//			
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}	
+//	}
+//	
+//	//測試是否拿到所有已購買電影資料
+//	@Test
+//	public void test2_6_GetGroceryOrderedList()
+//	{
+//		//拿到變數 buyListData 並確認電影資料
+//		// Use the timeout when navigating to a webpage
+//		driver.get("http://140.121.196.23:4107/shop.html");
+//		//等個幾秒 讓頁面Ajax loading好
+//		waitForPageLoaded();
+//		try {
+//			//拿到了
+//			JSONArray buyListData = new JSONArray((String)js.executeScript("return JSON.stringify(buyListData);"));
+//			System.out.println((String)js.executeScript("return JSON.stringify(buyListData);"));
+//			//如果第3筆資料的電影名稱是 "妖貓傳" ，就通過
+//			Assert.assertTrue(buyListData.getJSONObject(buyListData.length()).getString("MovieName").contains("妖貓傳"));
+//				
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}	
+//	}
+//	
+//	
 	@After
 	public void afterTest() 
 	{
